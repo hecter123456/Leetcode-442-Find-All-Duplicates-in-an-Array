83f3d6ee23130a526f182644e2beaa6d8017e442
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-09 15:44:21 +0800

    pass testcase: testDuplicate by using set

--- Find All Duplicates in an Array.py
+++ Find All Duplicates in an Array.py
@@ -4,11 +4,22 @@ class unitest(unittest.TestCase):
     def testNone(self):
         inputList = []
         self.assertEqual(Solution().findDuplicates(inputList),[])
-
+    def testDuplicates(self):
+        inputList = [4,3,2,7,8,2,3,1]
+        ans = [2,3]
+        self.assertEqual(Solution().findDuplicates(inputList),ans)
 class Solution(object):
     def findDuplicates(self, nums):
+        ans = []
         if nums == []:
-            return []
+            return ans
+        searchset = set() 
+        for node in nums:
+            if node in searchset:
+                ans.append(node)
+            else:
+                searchset.add(node)
+        return ans
 
 if __name__ == '__main__':
     unittest.main()
